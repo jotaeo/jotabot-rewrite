3cb1fde2a90f902537038f7680826bca83e5dbae
jotaeo <37418589+jotaeo@users.noreply.github.com>
2018-05-05 16:33:19 -0400

    Update jotabot.py

--- jotabot.py
+++ jotabot.py
@@ -11,26 +11,6 @@ async def test(ctx):
     await ctx.send("test")
 
 
-@bot.command()
-async def say(ctx, * ,msg):
-
-    await ctx.send (msg)
-
-@bot.command ()
-async def math (ctx,a:int,b,c:int):
-    if b== '+':
-        await ctx.send (a+c)
-    if b== "-":
-        await ctx.send (a-c)
-    if b== "*":
-        await ctx.send (a*c)
-    if b== "/":
-        await ctx.send (a/c)
-
-
-@bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send('{0} joined on {0.joined_at}'.format(member))
 
 
 

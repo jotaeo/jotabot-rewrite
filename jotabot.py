d922cd32eedb23d375272eeed643c9d51cd8bb8f
deepakraj-tc <deepakrajko14@gmail.com>
2018-05-06 20:58:13 +0530

    Update jotabot.py

--- jotabot.py
+++ jotabot.py
@@ -6,7 +6,14 @@ import asyncio
 bot = commands.Bot(command_prefix="j!")
 TOKEN = 'NDI3MTcwMjk1MjI5NDQ4MTk1.DZgpUA.IVDG3A2mb3owOEKJ0Ez1Frup7YE'
 
-@bot.command ()
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
 async def test(ctx):
     await ctx.send("test")
 
